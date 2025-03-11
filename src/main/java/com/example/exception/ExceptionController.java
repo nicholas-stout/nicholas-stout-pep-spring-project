11b58e968623ec2e4a3beb79f8c84a2a4194e2ea
Nick Stout <stout.nicholas2@gmail.com>
2025-03-11 16:37:43 +0000

    Finished POST register handler

---
+++ src/main/java/com/example/exception/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(DuplicateUsernameException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDuplicateUsername(DuplicateUsernameException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(AccountRegistrationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleUnsuccessfulAccountRegistration(AccountRegistrationException e) {
+        return e.getMessage();
+    }
+}
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Account;
+import com.example.exception.AccountRegistrationException;
+import com.example.exception.DuplicateUsernameException;
 import com.example.repository.AccountRepository;
 
 @Service
@@ -12,17 +14,44 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
-    // @Autowired
-    // public AccountService(AccountRepository accountRepository) {
-    //     this.accountRepository = accountRepository;
-    // }
-
     /**
-     * TODO: Handle input validation
      * This method will tell the account repo to save a new account
      * @param account the Account we wish to register
      */
     public void register(Account account) {
+        validateAccount(account);
         accountRepository.save(account);
+    }
+
+    private void validateAccount(Account account) {
+        if (!usernameAvailable(account.getUsername())) {
+            throw new DuplicateUsernameException(
+                    "An account with this username already exists. " +
+                            "Please choose a different username."
+            );
+        }
+
+        if (!isValidUsernameAndPassword(account.getUsername(), account.getPassword())) {
+            throw new AccountRegistrationException(
+                    "Your username must not be blank and your password " +
+                            "must be at least 4 characters."
+            );
+        }
+    }
+
+    private boolean usernameAvailable(String username) {
+        return accountRepository.findByUsername(username).isEmpty();
+    }
+
+    private boolean isValidUsernameAndPassword(String username, String password) {
+        return isValidUsername(username) && isValidPassword(password);
+    }
+
+    private boolean isValidUsername(String username) {
+        return !username.isEmpty();
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() > 4;
     }
 }
