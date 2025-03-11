da57b27d5b5b47421278e9966f16fabd3b19cc1b
Nick Stout <stout.nicholas2@gmail.com>
2025-03-11 15:22:30 +0000

    Passed registerUserSuccessful() Test

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    // Account repository that this class will use
+    @Autowired
+    private AccountRepository accountRepository;
+
+    // @Autowired
+    // public AccountService(AccountRepository accountRepository) {
+    //     this.accountRepository = accountRepository;
+    // }
+
+    /**
+     * TODO: Handle input validation
+     * This method will tell the account repo to save a new account
+     * @param account the Account we wish to register
+     */
+    public void register(Account account) {
+        accountRepository.save(account);
+    }
 }
